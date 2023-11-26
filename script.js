f5a46372c81c5e86b35b1a420507183a84fabba1
Caglar Kilic <73300418+CaglarKilic@users.noreply.github.com>
2023-11-26 23:04:51 +0300

    Added input form to sidebar for now.

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${pages} pages, ` + (this.read ? 'read' : 'not read yet')
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.info = function () {
+            return `${this.title} by ${this.author}, ${pages} pages, ` + (this.read ? 'read' : 'not read yet');
+        };
     }
-}
-
-function addBookToLibrary(book) {
-    
 }
