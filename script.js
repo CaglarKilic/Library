9d9587753fbf2fc22a7606614601a350291635a0
Caglar Kilic <73300418+CaglarKilic@users.noreply.github.com>
2023-11-28 19:50:18 +0300

    Finished first part.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 const form = document.forms[0];
-const table = document.querySelector('table');
+const table = document.querySelector('tbody');
 
 class Book {
     constructor(title, author, pages, read) {
@@ -27,10 +27,34 @@ function addBook(event) {
         elements['status'].checked
     );
 
+    myLibrary.push(book)
+
     row.insertCell().textContent = book.title;
     row.insertCell().textContent = book.author;
     row.insertCell().textContent = book.pages;
-    row.insertCell().textContent = book.status ? 'read' : 'not read';
-    
-    // form.reset();
+    row.insertCell().textContent = book.read ? 'read' : 'not read';
+
+    const remove = document.createElement('button');
+    const change = document.createElement('button');
+    remove.innerHTML = `<img src="icons/remove.svg">`;
+    change.innerHTML = `<img src="icons/change.svg">`;
+    remove.addEventListener('click', removeItem);
+    change.addEventListener('click', changeStatus);
+
+    row.insertCell().append(change, remove)    
+    form.reset();
+}
+
+function removeItem() {
+    const row = this.parentElement.parentElement;
+    myLibrary.splice(row.rowIndex - 1, 1);
+    row.remove();
+}
+
+function changeStatus() {
+    const row = this.parentElement.parentElement;
+    const index = row.rowIndex - 1;
+    const cell = row.cells[3];
+    myLibrary[index].read = !myLibrary[index].read;
+    cell.textContent = myLibrary[index].read ? 'read' : 'not read';
 }
